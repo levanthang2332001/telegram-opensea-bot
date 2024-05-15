8a89edcff836ee45e9e808871440c2094cdb913f
Văn Thắng <vanthang23032001@gmail.com>
2024-05-15 16:54:20 +0700

    feat: Add callback_query function

--- bot.ts
+++ bot.ts
@@ -8,38 +8,73 @@ if(!process.env.TELEGRAM_BOT_TOKEN) {
 }
 
 const TOKEN: string = process.env.TELEGRAM_BOT_TOKEN || '';
-
 const bot = new TelegramBot(TOKEN, {polling: true});
+
+interface ChatState {
+    waitingForAddress?: boolean;
+    waitingForCollection?: boolean;
+}
+
+const chatStates: Record<number, ChatState> = {};
 
 const myCommands = [
     {command: '/start', description: 'Start the bot'},
     {command: '/help', description: 'Get help'}
 ]
 
-bot.on('text', (msg: Message) => {
+const InlineKeyboardButtons: InlineKeyboardButton[][] = [
+    [{text: 'Address', callback_data: '/address'}, {text: 'Collection ', callback_data: '/collection'}],
+];
+
+const opts = {
+    reply_markup: {
+        inline_keyboard: InlineKeyboardButtons
+    }
+};
+
+bot.onText(/\/start/, (msg: Message) => {
     const chatId = msg.chat.id;
-    const welcomeMessage = 'Welcome! Choose an option:';
-
-    const InlineKeyboardButtons: InlineKeyboardButton[][] = [
-        [{text: 'Google', url: 'https://www.google.com', callback_data: 'google'}],
-        [{text: 'Bing', url: 'https://www.bing.com'}]
-    ];
-
-    const opts = {
-        reply_markup: {
-            inline_keyboard: InlineKeyboardButtons
-        }
-    };
-    
-    bot.sendMessage(chatId,welcomeMessage, opts);
+    bot.sendMessage(chatId, 'Welcome! Choose an option:', opts);
 });
 
+bot.onText(/\/address/, (msg: Message) => {
+    const chatId = msg.chat.id;
+    chatStates[chatId] = {waitingForAddress: true};
+    bot.sendMessage(chatId, 'Enter your address:');
+});
 
+bot.onText(/\/collection/, (msg: Message) => {
+    const chatId = msg.chat.id;
+    chatStates[chatId] = {...chatStates[chatId] ,waitingForCollection: true};
+    bot.sendMessage(chatId, 'Enter your collection');
+});
 
-// bot.onText(/\/start/, (msg: Message) => {
-//     const chatId = msg.chat.id;
-//     bot.sendMessage(chatId, 'You started the bot.');
-// });
+bot.on('message', (msg: Message) => {
+    const chatId = msg.chat.id;
+
+    if(chatStates[chatId]?.waitingForAddress) {
+        const address = msg.text;
+        chatStates[chatId] = {waitingForAddress: false};
+        bot.sendMessage(chatId, `Your address is: ${address}`);
+    } else if(chatStates[chatId]?.waitingForCollection) {
+        const collection = msg.text;
+        chatStates[chatId] = {waitingForCollection: false};
+        bot.sendMessage(chatId, `Your collection is: ${collection}`);
+    
+    }
+}); 
+
+bot.on('callback_query', (query) => {
+    const message = query.message;
+    const data = query.data;
+
+    console.log(data)
+
+    if(data === '/address' && message) {
+        bot.sendMessage(message.chat.id, `You clicked on ${data}`);
+    };
+
+});
 
 
 bot.setMyCommands(myCommands);
