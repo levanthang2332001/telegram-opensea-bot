0c51f7b2176e7e30b4a596961c785678dd382eea
vanthang559 <vanthang23032001@gmail.com>
2024-09-11 00:13:50 +0700

    stash2 code

--- src/types/message.ts
+++ src/types/message.ts
@@ -1,4 +1,4 @@
-const messageOfNetwork = "<b>Click on the network to select the chain</b>"
+const messageOfNetworks = "<b>Click on the network to select the chain</b>"
 
 const networks = [
     ["Ethereum", "Bsc", "Polygon"],
@@ -6,4 +6,4 @@ const networks = [
     ["Arbitrum"]
 ];
 
-export { messageOfNetwork, networks }
+export { messageOfNetworks, networks }
